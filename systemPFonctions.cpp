81e79046b8b3010d411cd3de8c326169da129614
owazae <39860768+owazae@users.noreply.github.com>
2019-04-06 00:41:58 +0200

    Update systemPFonctions.cpp

--- systemPFonctions.cpp
+++ systemPFonctions.cpp
@@ -1,22 +1,34 @@
 #include "systemP.h"
 
-void InitGrille(int x, int y){
-    Case grille[x][y];
+void afficheGrille(Case (&grille)[nL][nl]){
+    for(int i=0; i<nL; i++){
+        for(int j=0; j<nl; j++){
+            if (grille[i][j].espece == 0) cout << " 0 ";
+            if (grille[i][j].espece == 1) cout << " R ";
+            if (grille[i][j].espece == 2) cout << " L ";
+        }
+        cout << " " << endl;
+    cout << endl;
+    }
+}
+
+void InitGrille(Case (&grille)[nL][nl]){
     int randomNombre;
-    for(int i=0; i<x; i++){
-        for(int j=0; i<y; j++){
-            grille[i][j].coord.x=i;
-            grille[i][j].coord.y=j;
-            randomNombre = rand() % 100 + 1;
-            if(randomNombre <=20){ //lapin
-                grille[i][j].animal = lapin;
+    for(int i=0; i<nL; i++){
+        for(int j=0; j<nl; j++){
+            grille[i][j].coord.x=j;
+            grille[i][j].coord.y=i;
+            grille[i][j].deplace = false;
+            randomNombre = rand() % 101;
+            if(randomNombre <=20){
+                grille[i][j].espece = lapin;
             }
-            else if(randomNombre >=93){ //renard
+            else if(randomNombre >=93){
+                grille[i][j].espece = renard;
                 grille[i][j].food = FoodInit;
-                grille[i][j].animal = renard;
             }
             else{
-                grille[i][j].animal = nul;
+                grille[i][j].espece = nul;
             }
         }
     }
