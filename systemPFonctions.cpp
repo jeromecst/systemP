325016f2155161fe6ca5a21acc5a40ef54abacc5
owazae <39860768+owazae@users.noreply.github.com>
2019-04-05 19:39:34 +0200

    Update systemPFonctions.cpp

--- systemPFonctions.cpp
+++ systemPFonctions.cpp
@@ -1,1 +1,23 @@
 #include "systemP.h"
+
+void InitGrille(int x, int y){
+    Case grille[x][y];
+    int randomNombre;
+    for(int i=0; i<x; i++){
+        for(int j=0; i<y; j++){
+            grille[i][j].coord.x=i;
+            grille[i][j].coord.y=j;
+            randomNombre = rand() % 100 + 1;
+            if(randomNombre <=20){ //lapin
+                grille[i][j].animal = lapin;
+            }
+            else if(randomNombre >=93){ //renard
+                grille[i][j].food = FoodInit;
+                grille[i][j].animal = renard;
+            }
+            else{
+                grille[i][j].animal = nul;
+            }
+        }
+    }
+}
