99d60b9b68ca7965d2b6ebbb11f0905eab13be0a
owazae <39860768+owazae@users.noreply.github.com>
2019-04-06 16:11:31 +0200

    Update systemP.cpp

--- systemP.cpp
+++ systemP.cpp
@@ -3,6 +3,35 @@
 int main(){
     Case grille[nL][nl];
     srand (time(NULL));
+    
+    
     InitGrille(grille);
-    afficheGrille(grille);
+
+    int nbL=0, nbl=0, nbr=0, nbR=0;
+    
+    AfficheGrille(grille);
+
+    for(int i=0; i<nL; i++){
+        for(int j=0; j<nl ; j++){
+            if(grille[i][j].espece==lapin) nbl+=1;
+            if(grille[i][j].espece==renard) nbr+=1;
+        }
+    }
+
+    for(int i=0; i<60; i++) cout << "#";
+    cout << endl<< endl;
+
+    DeplacementLapin(grille);
+
+    for(int i=0; i<nL; i++){
+        for(int j=0; j<nl ; j++){
+            if(grille[i][j].espece==lapin) nbL+=1;
+            if(grille[i][j].espece==renard) nbR+=1;
+        }
+    }
+
+    AfficheGrille(grille);
+
+    cout  << "lapins : avant : " << nbl << "  après : " << nbL << endl;
+    cout  << "renards : avant : " << nbr << "  après : " << nbR << endl;
 }
